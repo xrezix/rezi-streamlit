98aa5f112728e9b66f9e4848e13fe881fa52ed69
rezipradana <89156606+xrezix@users.noreply.github.com>
2023-05-16 13:36:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def main():
        """)
         a,b,c = st.columns([0.2,0.6,0.2])
         with b:
-        st.image("banner-picture.jpeg", use_column_width=True)
+         st.image("banner-picture.jpeg", use_column_width=True)
 
 # description about the project and code files       
 st.subheader("🧾Description:")
