75d7cdcb988f4b1e655cfdf47740e949ccb93fdb
rezipradana <89156606+xrezix@users.noreply.github.com>
2023-05-16 13:35:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def main():
        [Kaggle](https://www.kaggle.com/avikumart) 
        """)
         a,b,c = st.columns([0.2,0.6,0.2])
-    with b:
+        with b:
         st.image("banner-picture.jpeg", use_column_width=True)
 
 # description about the project and code files       
@@ -119,4 +119,4 @@ st.markdown("Please find GitHub repository link of project: [Click Here](https:/
   
 # run the main function        
 if __name__ == '__main__':
-    main()
+main()
